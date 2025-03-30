aeb8d4575d23c2cbaa798e9b5dea9c68ae47f7eb
Vsumavane <vsumavane@outlook.com>
2025-03-30 14:19:22 +0530

    ref: add Homepage and editor page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Editor from "./pages/Editor";
 
 function App() {
-
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+        {/* Editor Page */}
+        <Route path="/editor" element={<Editor />} />
+      </Routes>
+    </Router>
   );
 }
 
